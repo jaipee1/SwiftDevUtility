2ab17330d18436c3bad2c16e03e18baaaa5edd51
jaipee <mac@Macs-Mac-mini.local>
2020-09-09 20:53:44 +0530

    initial commit

---
+++ SwiftDevUtility/SwiftDevUtility.h
@@ -0,0 +1,19 @@
+//
+//  SwiftDevUtility.h
+//  SwiftDevUtility
+//
+//  Created by jaipee on 09/09/20.
+//  Copyright © 2020 JaiPrakashYadav. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SwiftDevUtility.
+FOUNDATION_EXPORT double SwiftDevUtilityVersionNumber;
+
+//! Project version string for SwiftDevUtility.
+FOUNDATION_EXPORT const unsigned char SwiftDevUtilityVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftDevUtility/PublicHeader.h>
+
+
